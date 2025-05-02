392e5ac01e07984b0e366a667c36eab40500b1c1
VivekBansal <kumarv98133@gmail.com>
2025-05-02 22:07:41 +0530

    Add health checker

--- src/main/java/org/example/medrembackend/Controller/HealthChecker.java
+++ src/main/java/org/example/medrembackend/Controller/HealthChecker.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthChecker {
 
+
     @GetMapping("/health-check")
     public String healthChecker() {
         return "OK";
